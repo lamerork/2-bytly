4117dfccff4d90565089582345b3eb06ce2f03c5
Anton Ozerov <lamerork@gmail.com>
2023-05-31 18:27:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+MAINLINK = os.getenv('MAINLINK')
+
+def shorten_link(token, link):
+
+    headers = {
+    'Authorization': token,
+    'Content-Type': 'application/json',
+    'long_url': link
+    }
+
+    data = {"long_url": link}
+    print(data)
+    response = requests.post(MAINLINK, headers=headers, json=data)
+    response.raise_for_status()
+    bitlink = response.json()
+
+    return bitlink['id']
+
+def get_link():
+    user_link = input('Введите ссылку: ')
+    return user_link
+
+def count_clicks(token, link):
+
+    headers = {
+    'Authorization': token,
+    'Content-Type': 'application/json',
+    }
+
+    params = {
+        'unit': 'day',
+        'units': '-1'
+    }
+
+    response = requests.get(f'{MAINLINK}{link}/clicks/summary', params=params, headers=headers)
+    print(response.url)
+    response.raise_for_status()
+    click_count = response.json()
+
+    return click_count['total_clicks']
+
+def is_bitlink(url):
+    
+    if url[:6] == 'https:' or url[:5] == 'http:':
+        return True
+    return False
+
+def main():
+
+    user_link = get_link()
+
+    try:
+        
+        if is_bitlink(user_link):
+            print('Битлинк:', shorten_link(TOKEN, user_link))
+
+        else:
+            print('Клики:', count_clicks(TOKEN, user_link))
+
+    except requests.exceptions.HTTPError:
+        print("Ошибка! Неверная ссылка")
+
+if __name__ == '__main__':
+    
+    main()
